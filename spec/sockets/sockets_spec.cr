3e4c96f0aa2d425f3aca58415ac141523440cf1e
noraj <alexandre.zanni@europe.com>
2024-04-11 21:28:33 +0200

    Lint/SpecFilename + fix constant check

--- spec/sockets/sockets_spec.cr
+++ spec/sockets/sockets_spec.cr
@@ -9,35 +9,35 @@ describe "Miniss::Sockets" do
   describe "#tcpv4" do
     it "returns Array(Socket)" do
       sockets = Miniss::Sockets.new
-      sockets.tcpv4.should be_a(Array(Socket))
+      sockets.tcpv4.should be_a(Array(Miniss::Socket))
     end
   end
 
   describe "#tcpv6" do
     it "returns Array(Socket)" do
       sockets = Miniss::Sockets.new
-      sockets.tcpv6.should be_a(Array(Socket))
+      sockets.tcpv6.should be_a(Array(Miniss::Socket))
     end
   end
 
   describe "#udppv4" do
     it "returns Array(Socket)" do
       sockets = Miniss::Sockets.new
-      sockets.udpv4.should be_a(Array(Socket))
+      sockets.udpv4.should be_a(Array(Miniss::Socket))
     end
   end
 
   describe "#udppv6" do
     it "returns Array(Socket)" do
       sockets = Miniss::Sockets.new
-      sockets.udpv6.should be_a(Array(Socket))
+      sockets.udpv6.should be_a(Array(Miniss::Socket))
     end
   end
 
   describe "#all" do
     it "returns Array(Socket)" do
       sockets = Miniss::Sockets.new
-      sockets.all.should be_a(Array(Socket))
+      sockets.all.should be_a(Array(Miniss::Socket))
     end
   end
 end
