cc4bf1115a79e000b83c26e7749827ebf7bc3b5f
noraj <alexandre.zanni@europe.com>
2024-04-11 21:25:35 +0200

    fix typo raising error in tests

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -8,7 +8,7 @@ def create_test_socket_tcp_ipv4
   so
 end
 
-def create_test_socket_udp_ivp4
+def create_test_socket_udp_ipv4
   so = Miniss::Socket.new(:udp, 4_u8)
   line = " 2807: 5B01A8C0:0044 00000000:0000 07 00000000:00000000 00:00000000 00000000   981        0 17170 2 00000000da060629 0         " # udp  192.168.1.91:68                                 0.0.0.0:0                                       CLOSE       systemd-network (981)
   so.parse_line(line)
@@ -22,7 +22,7 @@ def create_test_socket_tcp_ipv6
   so
 end
 
-def create_test_socket_udp_ivp6
+def create_test_socket_udp_ipv6
   so = Miniss::Socket.new(:udp, 6_u8)
   line = " 3285: 000080FE00000000FF005450B6AD1DFE:0222 00000000000000000000000000000000:0000 07 00000000:00000000 00:00000000 00000000   981        0 17289 2 0000000041d378f3 0" # udp  [fe80::5054:ff:fe1d:adb6]:546                   [::]:0                                          CLOSE       systemd-network (981)
   so.parse_line(line)
